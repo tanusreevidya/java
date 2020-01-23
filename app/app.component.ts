6c6c81e91b1d07d7d94f44ce160d604812704d2f
R.Tanu Sree Vidya <59555114+tanusreevidya@users.noreply.github.com>
2020-01-23 18:01:01 +0530

    23 jan

---
+++ app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+//import { User } from './models/user';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'stockholder';
+
+ // user = new User(101, 'tanu sree','tanu@gmail.com',999999999);
+
+  // callMe(){
+  //   alert('welcome :' + this.user.name);
+  // }  
+  //user1=USERSS;
+}
---
+++ app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RegisterComponent } from './user/register/register.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterComponent,
+    HeroesComponent,
+    HeroDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
