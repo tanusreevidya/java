44f971fc7d195e225b9b3e75f53821e3d4fcbdf9
R.Tanu Sree Vidya <59555114+tanusreevidya@users.noreply.github.com>
2020-02-19 18:13:10 +0530

    19 feb

---
+++ 19 feb/springrestservice/src/net/javaonline/spring/restful/controller/HelloWorldService.java
@@ -0,0 +1,33 @@
+package net.javaonline.spring.restful.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldService {
+	
+/*	
+	@RequestMapping
+    public String helloMethod1(@RequestParam(value="name", defaultValue="World") String name) {
+        
+    	  return   "Hello  "+ name;
+    }
+  */ 
+	
+	@RequestMapping(value="/{firstName}")
+    public String helloMethod1(@PathVariable String firstName) {
+        System.out.println("name:::"+firstName);
+    	  return   "Hello  "+ firstName;
+    }
+	
+	@RequestMapping(value = "/{firstName}/{lastName}", method = RequestMethod.GET)
+   	public String helloMethod2(@PathVariable String firstName, @PathVariable String lastName) {
+   		return "Hello "+ firstName + " " + lastName;	
+    	}    
+    
+}
--- README.md
+++ README.md
@@ -176,3 +176,9 @@ Filter
 
  ## Day 41 (18-02-2020)
  pl/sql
+
+
+ ## Day 42 (19-02-2020)
+ spring rest
+ spring boot
+ 
